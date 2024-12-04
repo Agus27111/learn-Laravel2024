ca4bdeba3058df08a200d799038ccbf0d059dd43
Agus27111 <agussetiawanbelajar@gmail.com>
2024-12-04 21:19:10 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,8 @@
+
+
+@extends('template.layout')
+
+@section('content')
+    <h1>Blog</h1>
+    <p>Ini adalah halaman blog dengan judul {{ $title }}.</p>
+@endsection
---
+++ resources/views/template/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout</title>
+</head>
+<body>
+
+<div class="header">
+    <h1>Header</h1>
+</div>
+
+<div class="content">
+    @yield('content')
+</div>
+
+<div class="footer">
+    <h1>Footer</h1>
+</div>
+
+</body>
+</html>
