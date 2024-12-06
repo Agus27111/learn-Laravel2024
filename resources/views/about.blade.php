278190c72f1428254bc1f723d9de929d9c96d1b0
Agus27111 <agussetiawanbelajar@gmail.com>
2024-12-07 02:01:16 +0700

    add bootsrap

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('template.layout')
+
+
+@section('content')
+<div class="container">
+    <h1> "this is about page"</h1>
+</div>
+
+@endsection
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,6 +3,22 @@
 @extends('template.layout')
 
 @section('content')
-    <h1>Blog</h1>
-    <p>Ini adalah halaman blog dengan judul {{ $title }}.</p>
+<div class="container px-4 py-5" id="featured-3">
+
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+        @for ($i =0 ; $i < 3; $i++)
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Judul Blog {{$i+1}} </h3>
+        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <a href="#" class="icon-link">
+          Read
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+        @endfor
+    </div>
+  </div>
 @endsection
